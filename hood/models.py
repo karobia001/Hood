fe162ed6227f195b27e69f13befed84900a5e9a8
karobia001 <karobiamaina81@gmail.com>
2019-12-02 12:17:43 +0300

    creation of other models and addition of more objects

--- hood/models.py
+++ hood/models.py
@@ -1,23 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
-class Neighbourhood(models.Model):
-    name = models.CharField(max_length =30)
-    location = models.CharField(max_length =30)
-    occupant_no = models.IntegerField()
-    # admin = models.ForeignKey(User)
+class NeighborHood(models.Model):
+
+    neighborhood_image=models.ImageField(upload_to='neighborhoods',null=True)
+    name=models.CharField(max_length =40)
+    location=models.CharField(max_length=40)
+    occupants_count=models.PositiveIntegerField(blank=True,null=True)
+
     
+
+
+class Userprofile(models.Model):
+
+    profile_image=models.ImageField(upload_to='userprofiles',null=True)
+    user_name=models.OneToOneField(User,null = True,on_delete=models.CASCADE,related_name = "user")
+    neighborhood=models.ForeignKey(NeighborHood,on_delete=models.CASCADE,blank=True,null=True)
+    email=models.EmailField(blank=True,null=True)
+
+
+
     
-class User(models.Model):
-    name = models.CharField(max_length=30)
-    neighbourhood = models.ForeignKey(Neighbourhood)
-    email=models.CharField(max_length =60)
-    
-    
+
 class Business(models.Model):
-    business_name= models.CharField(max_length =30)
-    user= models.ForeignKey(User)
-    neighbourhood=models.ForeignKey(Neighbourhood)
-    business_email=models.EmailField()
+    business_image=models.ImageField(upload_to='businesses',null=True)
+    business_name=models.CharField(max_length =30)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
+    neighborhood=models.ForeignKey(NeighborHood,on_delete=models.CASCADE,blank=True,null=True)
+    email=models.EmailField(blank=True,null=True)
+
     
+
+
+
+class Post(models.Model):
+    post_image=models.ImageField(upload_to='posts',null=True)
+    owner=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    title=models.CharField(max_length =30)
+    neighborhood=models.ForeignKey(NeighborHood,on_delete=models.CASCADE,blank=True,null=True)
+    post=models.TextField()
+
+
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,blank=True, on_delete=models.CASCADE,null=True,related_name='comment')
+    commenter=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    comment=models.TextField(max_length =30)
+
+
+
+    
+
